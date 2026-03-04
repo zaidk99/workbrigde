f539803ab7af487a979ea081078fb3c03a9aecdb
zaidk99 <mdzaidkhan129@gmail.com>
2026-03-04 21:15:46 +0530

    clean imports

--- backend/src/modules/serviceRequests/serviceRequest.controller.ts
+++ backend/src/modules/serviceRequests/serviceRequest.controller.ts
@@ -5,20 +5,30 @@ export const createServiceRequestController = async (
   req: Request,
   res: Response,
 ) => {
+  
+  if (!req.user) {
+  res.status(401).json({ message: "Unauthorized" });
+  return;
+}
   // get hold of inputs
-  const { client_user_id, title, description } = req.body;
-
+  const client_user_id = req.user.id;
+  const {title, description } = req.body;
+  
+  if(!client_user_id || !title || !description){
+    res.status(400).json({message: "all fields are required"});
+    return;
+  }
   try {
     const result = await createServiceRequestsbyClientService({
       client_user_id,
       title,
       description,
     });
-    res.status(200).json({
+    res.status(201).json({
         message : 'service request created successfully',
         sr : result,
     })
   } catch (error) {
-     res.status(500).json({ message: "Internal server error" });
+     res.status(500).json({ message: "internal server error" });
   }
 };
--- backend/src/modules/serviceRequests/serviceRequest.routes.ts
+++ backend/src/modules/serviceRequests/serviceRequest.routes.ts
@@ -1,15 +1,17 @@
 import express from "express";
-import { Router } from "express";
 import { authorizeRoles } from "../../middleware/authorizeRoles";
 import { authenticate } from "../../middleware/authenticate";
+import { createServiceRequestController } from "./serviceRequest.controller";
 
 const router = express.Router();
 
 
-router.post("/service-requests",authenticate,authorizeRoles('client'));
-// get all req by logged in client
-router.get("/service-requests",authenticate,authorizeRoles('client'));
-// get specific one req by its id 
-router.get("service-requests/:id",authenticate,authorizeRoles('client'));
+router.post("/service-requests",authenticate,authorizeRoles('client'),createServiceRequestController);
 // admin gets all req from all clients
 router.get("/service-requests/all",authenticate,authorizeRoles('admin'));
+// get all req by logged in client
+router.get("/service-requests/:id",authenticate,authorizeRoles('client'));
+router.get("/service-requests",authenticate,authorizeRoles('client'));
+
+
+
