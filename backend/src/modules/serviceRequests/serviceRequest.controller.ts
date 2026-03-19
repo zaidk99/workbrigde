6016ceadde4d9107657b05a354e7437aeb25bd1d
zaidk99 <mdzaidkhan129@gmail.com>
2026-03-20 00:12:49 +0530

    building service request status workflow

--- backend/src/modules/serviceRequests/serviceRequest.controller.ts
+++ backend/src/modules/serviceRequests/serviceRequest.controller.ts
@@ -1,21 +1,26 @@
 import { Request, Response } from "express";
-import { createServiceRequestsbyClientService, getallServiceRequestsService, getallSRforSpecificClientServiceRequestService, getServiceRequestsByidinitiaterclientandadminservice } from "./serviceRequest.service";
+import {
+  approverejectSeriviceRequestAdminonly,
+  createServiceRequestsbyClientService,
+  getallServiceRequestsService,
+  getallSRforSpecificClientServiceRequestService,
+  getServiceRequestsByidinitiaterclientandadminservice,
+} from "./serviceRequest.service";
 
 export const createServiceRequestController = async (
   req: Request,
   res: Response,
 ) => {
-  
   if (!req.user) {
-  res.status(401).json({ message: "Unauthorized" });
-  return;
-}
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
   // get hold of inputs
   const client_user_id = req.user.id;
-  const {title, description } = req.body;
-  
-  if(!client_user_id || !title || !description){
-    res.status(400).json({message: "all fields are required"});
+  const { title, description } = req.body;
+
+  if (!client_user_id || !title || !description) {
+    res.status(400).json({ message: "all fields are required" });
     return;
   }
   try {
@@ -25,70 +30,107 @@ export const createServiceRequestController = async (
       description,
     });
     res.status(201).json({
-        message : 'service request created successfully',
-        sr : result,
-    })
+      message: "service request created successfully",
+      sr: result,
+    });
   } catch (error) {
-     res.status(500).json({ message: "internal server error" });
+    res.status(500).json({ message: "internal server error" });
   }
 };
 
-
-export const getallSRforSpecificClientServiceRequestController = async(req:Request,res:Response)=>{
-  if(!req.user){
-    res.status(401).json({message:"Unauthorized"});
+export const getallSRforSpecificClientServiceRequestController = async (
+  req: Request,
+  res: Response,
+) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthorized" });
     return;
   }
   const client_user_id = req.user.id;
   try {
-    const result = await getallSRforSpecificClientServiceRequestService(client_user_id);
+    const result =
+      await getallSRforSpecificClientServiceRequestService(client_user_id);
     res.status(200).json({
-      message:"got all the service requests by the client",
-      sr:result,
-    })
+      message: "got all the service requests by the client",
+      sr: result,
+    });
   } catch (error) {
-    res.status(500).json({message:"internal server error"});
+    res.status(500).json({ message: "internal server error" });
   }
 };
 
-export const getallServiceRequestsController = async(req:Request ,res:Response)=>{
-  if(!req.user){
-    res.status(401).json({message:"Unauthorized"});
+export const getallServiceRequestsController = async (
+  req: Request,
+  res: Response,
+) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthorized" });
     return;
   }
   try {
     const result = await getallServiceRequestsService();
     res.status(200).json({
-      message:"got all the service requests made by all clients",
-      sr:result,
-    })
+      message: "got all the service requests made by all clients",
+      sr: result,
+    });
   } catch (error) {
-    res.status(500).json({message:"internal server error"});
+    res.status(500).json({ message: "internal server error" });
   }
 };
 
-export const getServiceRequestsByidinitiaterclientandadminController = async(req:Request , res:Response)=>{
-  if(!req.user){
-    res.status(401).json({message:"unauthorized"});
+export const getServiceRequestsByidinitiaterclientandadminController = async (
+  req: Request,
+  res: Response,
+) => {
+  if (!req.user) {
+    res.status(401).json({ message: "unauthorized" });
     return;
   }
   const getServiceRequestId = req.params.id as string;
   try {
-    const result = await getServiceRequestsByidinitiaterclientandadminservice(getServiceRequestId);
-    if(!result){
-      res.status(404).json({message:"service request not found"});
+    const result =
+      await getServiceRequestsByidinitiaterclientandadminservice(
+        getServiceRequestId,
+      );
+    if (!result) {
+      res.status(404).json({ message: "service request not found" });
       return;
     }
     res.status(200).json({
-      message:"got the speicifc id service request ",
-      sr:result,
-    })
+      message: "got the speicifc id service request ",
+      sr: result,
+    });
   } catch (error) {
-    res.status(500).json({message:"internal server error"});
+    res.status(500).json({ message: "internal server error" });
   }
 };
 
-export const approverejectSeriviceRequestAdminonlyController = async(req:Request , res:Response)=>{
-  if()
-}
+export const approverejectSeriviceRequestAdminonlyController = async (
+  req: Request,
+  res: Response
+) => {
+  if(!req.user){
+    res.status(401).json({message: "unauthorized"});
+    return;
+  }
+  const id = req.params;
+  const status = req.body;
 
+  const validStatus = ['pending','accepted','rejected'];
+  if(!validStatus.includes(status)){
+    res.status(400).json({message:"Invalid status value"});
+    return;
+  }
+
+  try {
+    const result = await approverejectSeriviceRequestAdminonly(id,status);
+     if(!)
+    }
+    
+  } catch (error) {
+    res.status(500).json({message:"internal server error"});
+    
+  }
+
+
+};
