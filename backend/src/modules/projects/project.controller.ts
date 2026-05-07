f0fc8a1a4a0ad237673eeb8300bcece7de2acfe9
zaidk99 <mdzaidkhan129@gmail.com>
2026-05-07 23:25:48 +0530

    buidling project service

--- backend/src/modules/projects/project.controller.ts
+++ backend/src/modules/projects/project.controller.ts
@@ -1,16 +1,23 @@
 import express from "express";
 import { pool } from "../../config/db";
 
-interface inputForprojectServie{
-    id:string,
+interface inputForprojectServie {
+  id: string;
 }
 interface outputForprojectService {
-   id:string,
-   name:string,
-   description:string,
-   client_user_id:string,
-   service_request_id:string,
-   status:string,
-   created_at:Date,
-   updated_at:Date, 
+  id: string;
+  name: string;
+  description: string;
+  client_user_id: string;
+  service_request_id: string;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
 }
+
+export const getallProjectsinitiatoradminOnly = async (
+  inputForprojectServi,
+): Promise<outputForprojectService[]> => {
+
+    
+};
