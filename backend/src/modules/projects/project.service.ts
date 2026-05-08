bb5e2375bbc4c21862310d1a765cdada08e5bf03
zaidk99 <mdzaidkhan129@gmail.com>
2026-05-08 20:42:14 +0530

    getting projects called by admin

---
+++ backend/src/modules/projects/project.service.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { pool } from "../../config/db";
+
+interface inputForprojectServie {
+  id: string;
+}
+interface outputForprojectService {
+  id: string;
+  name: string;
+  description: string;
+  client_user_id: string;
+  service_request_id: string;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export const getallProjectsinitiatoradminOnly = async (
+  id:string,
+) : Promise <outputForprojectService[]> => {
+
+    const result = await pool.query(`SELECT 
+        
+        p.id,
+        p.name,
+        p.description,
+        p.client_user_id,
+        p.service_request_id,
+        p.status,
+        p.created_at,
+        p.updated_at,
+        u.name AS client_name,
+        u.email AS client_email
+
+        FROM projects p JOIN users ON u.id = p.client_user_id
+        ORDER BY p.created_at DESC
+        
+        `);
+
+        return result.rows;
+
+};
