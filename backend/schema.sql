f0088f471100d1b83c8fdc821e071861745de774
zaidk99 <mdzaidkhan129@gmail.com>
2026-07-07 23:28:15 +0530

    build project files service changes to schema needed

--- backend/schema.sql
+++ backend/schema.sql
@@ -51,6 +51,7 @@ CREATE TABLE IF NOT EXISTS projects(
 );
 
 -- project files table  -- schema chages needed for project file uploads 
+-- add of filelds --> uploaded_by , user_id are needed
 CREATE TABLE IF NOT EXISTS project_files(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     project_id UUID NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
