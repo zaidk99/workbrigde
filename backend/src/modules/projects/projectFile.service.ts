01ebcda54bb20c65774de7ac5a9112023b0be165
zaidk99 <mdzaidkhan129@gmail.com>
2026-07-10 21:39:09 +0530

    add: service for uploading the project files

--- backend/src/modules/projects/projectFile.service.ts
+++ backend/src/modules/projects/projectFile.service.ts
@@ -3,46 +3,44 @@ import { pool } from "../../config/db";
 import multer = require("multer");
 
 interface uploadInput {
-    project_id : string,
-    original_file_name:string,
-    role:string,
-    user_id:string,
-    files: Express.Multer.File[]
+  project_id: string;
+  original_file_name: string;
+  role: string;
+  user_id: string;
+  files: Express.Multer.File[];
 }
 
 interface UploadToS3Result {
   objectKey: string;
   response: PutObjectCommandOutput;
 }
-export const uploadProjectFile = async({
-    project_id,
-    original_file_name,
-    role,
-    user_id,
-    files,
-
-}:uploadInput):Promise<UploadToS3Result>=>{
-    
-    const checkProject = await pool.query(`
+export const uploadProjectFile = async ({
+  project_id,
+  original_file_name,
+  role,
+  user_id,
+  files,
+}: uploadInput): Promise<UploadToS3Result> => {
+  const checkProject = await pool.query(
+    `
         SELECT * FROM projects WHERE id = $1;
-        `,[project_id]);
-    
-    if(checkProject.rows.length === 0){
-        throw new Error("project does not exist");
+        `,
+    [project_id],
+  );
+
+  if (checkProject.rows.length === 0) {
+    throw new Error("project does not exist");
+  }
+
+  if (role == "client") {
+    const checkProjectOwner = await pool.query(
+      `
+            SELECT * FROM projects WHERE id = $1 AND client_user_id = $2;`,
+      [project_id, user_id],
+    );
+
+    if (checkProjectOwner.rows.length === 0) {
+      throw new Error("you are not authorized to upload to this project");
     }
-
-    if(role=='client'){
-        const checkProjectOwner = await pool.query(`
-            SELECT * FROM projects WHERE id = $1 AND client_user_id = $2;`,[project_id,user_id]);
-
-        if(checkProjectOwner.rows.length === 0){
-            throw new Error("you are not authorized to upload to this project");
-        };
-
-
-
-    
-
-    }
-
-}
+  }
+};
