c781511a4197e88e60c2bebec28cce07db785f8f
zaidk99 <mdzaidkhan129@gmail.com>
2026-07-18 20:12:26 +0530

    build:controller for the projectFile service

--- backend/src/modules/projects/projectFile.controller.ts
+++ backend/src/modules/projects/projectFile.controller.ts
@@ -2,5 +2,28 @@ import { Response , Request } from "express";
 
 
 export const uploadProjectFileController = async (res:Response , req:Request )=>{
+    if(!req.user){
+        res.status(401).json({
+            success:false,
+            message:"unauthorized"
+        })
+        return;
+    }
+
+    const user_role = req.user.role;
+    const user_id = req.user.id;
+    const {project_id} = req.params;
+    const files = req.files as Express.Multer.File[];
+
+    if(!files || files.length === 0){
+        res.status(400).json({
+            success:false,
+            message:"no files uploaded"
+        });
+        return;
+    }
+
+    await 
+    
   
 }
