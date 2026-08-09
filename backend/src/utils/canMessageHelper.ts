32c03392c4eb2abb6850392ecddb5d6e1779b5fc
zaidk99 <mdzaidkhan129@gmail.com>
2026-08-09 14:28:33 +0530

    build:helper function to set permission flow for users who can message whom

--- backend/src/utils/canMessageHelper.ts
+++ backend/src/utils/canMessageHelper.ts
@@ -1,28 +1,40 @@
-import { pool } from  "../config/db";
+import { pool } from "../config/db";
 
-export const canMessage = async(sender_id:string,sender_role:string,receiver_id:string)=>{
+export const canMessage = async (
+  sender_id: string,
+  sender_role: string,
+  receiver_id: string,
+) => {
+  const get_receiver_role = await pool.query(
+    `SELECT role FROM users WHERE id = $1`,
+    [receiver_id],
+  );
 
-        const get_receiver_role = await pool.query(`SELECT role FROM users WHERE id = $1`,[receiver_id]);
+  const receiver_role = get_receiver_role.rows[0]?.role;
+  console.log("receiver_role in helper function canMessage :", receiver_role);
 
-        const receiver_role = get_receiver_role.rows[0]?.role;
-        console.log("receiver_role in helper function canMessage :" , receiver_role);
+  if (!receiver_role) {
+    throw new Error("reciever not found");
+  }
 
+  if (sender_role === "admin" || receiver_role === "admin") {
+    return true;
+  }
 
-        if(!receiver_role){
-                throw new Error("reciever not found");
-        }
+  if (sender_role === "employee" && receiver_role === "employee") {
+    const result = await pool.query(
+      `
+                                SELECT EXISTS(SELECT 1 FROM project_employees 
+                                pe1 JOIN project_employees pe2  
+                                      ON pe1.project_id = pe2.project_id 
+                                WHERE pe1.employee_id = $1
+                                AND   pe2.employee_id = $2
+                                )`,
+      [sender_id, receiver_id],
+    );
 
-        if(sender_role === 'admin' || receiver_role === 'admin'){
-                return true;
-        } else {
-                
-        }
+ 
 
-
-
-
-
-
-
-
-} 
+    return result.rows[0].exists;
+  }
+};
