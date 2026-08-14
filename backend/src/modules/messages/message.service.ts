5f5ac8a8834b8f3ba9636244746816d8b722cb95
zaidk99 <mdzaidkhan129@gmail.com>
2026-08-14 23:07:55 +0530

    build:servie to send message to specific user_id

--- backend/src/modules/messages/message.service.ts
+++ backend/src/modules/messages/message.service.ts
@@ -2,7 +2,8 @@ import { pool } from "../../config/db";
 
 
 export const sendMessagetospecifiUserId = async()=>{
-    const sendMessage = await pool.query(``);
+    const sendMessage = await pool.query(`INSERT INTO `);
+
 }
 export const getMessagesForSpecificUserId = async()=>{
 
