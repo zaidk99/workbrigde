8936bc9f8a2d3de3506c155d02196525b0ebbbed
zaidk99 <mdzaidkhan129@gmail.com>
2026-08-19 20:48:10 +0530

    building the message service

--- backend/src/modules/messages/message.service.ts
+++ backend/src/modules/messages/message.service.ts
@@ -1,10 +1,18 @@
 import { pool } from "../../config/db";
 
 
-export const sendMessagetospecifiUserId = async()=>{
-    const sendMessage = await pool.query(`INSERT INTO `);
+export const sendMessagetospecifiUserId = async(
+    senderId: string,
+    receiverId: string,
+    content:string,
+)=>{
+    const receiver = await pool.query(`SELECT id, role FROM users WHERE id = $1`,[receiverId]);
+    if(receiver.rows.length === 0){
+        throw new Error(`receiver does not exist`)
+    }
 
 }
+
 export const getMessagesForSpecificUserId = async()=>{
 
 }
