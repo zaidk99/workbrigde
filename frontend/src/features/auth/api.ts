bcac1fda5603d1bf67ad3c3d058f4bae87e2e8e1
zaidk99 <mdzaidkhan129@gmail.com>
2026-09-08 23:33:50 +0530

    connect: auth login page to backend

--- frontend/src/features/auth/api.ts
+++ frontend/src/features/auth/api.ts
@@ -1,1 +1,9 @@
-import a
+import  {api}  from "../../api/client";
+import { LoginRequest , LoginResponse } from "../../types/auth";
+
+
+export const login = async(body: LoginRequest):Promise<LoginResponse>=>{
+    const response = await api.post<LoginResponse>("/auth/login",body);
+    return response.data;
+
+}
