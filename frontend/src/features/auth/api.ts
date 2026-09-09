052051954bb9549a9a04334699235b75a7672fe2
zaidk99 <mdzaidkhan129@gmail.com>
2026-09-09 19:45:49 +0530

    add:api call to ui for login

--- frontend/src/features/auth/api.ts
+++ frontend/src/features/auth/api.ts
@@ -1,9 +1,9 @@
 import  {api}  from "../../api/client";
-import { LoginRequest , LoginResponse } from "../../types/auth";
+import type { LoginRequest, LoginResponse } from "../../types/auth";
 
 
 export const login = async(body: LoginRequest):Promise<LoginResponse>=>{
     const response = await api.post<LoginResponse>("/auth/login",body);
     return response.data;
+};
 
-}
